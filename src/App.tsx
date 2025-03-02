205c1f6d671fdadb4346a914a6856712f441b7d4
Francilino-Netoo <francilinonetoo@gmail.com>
2025-03-02 13:17:49 -0300

    Ajustes no jogo da memória

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,110 @@ import Logo from "./assets/devmemory_logo.png";
 import RestartIcon from "./svgs/restart.svg";
 import { InfoItem } from "./components/infoitenm";
 import { Button } from "./button";
-import {GridItemType} from './types/gridetipes';
-import {items} from './data/items';
+import { GridItemType } from './types/gridetipes';
+import { items } from './data/items';
 import { GridItem } from "./components/GridItem";
+import { Tempo } from "./helpers/formtatime";
 
 const App = () => {
-  const [plauing, setPlaying] = useState<boolean>(false);
+  const [playing, setPlaying] = useState<boolean>(false);
   const [timeElapsed, setTimeElapsed] = useState<number>(0);
   const [moveCount, setMoveCount] = useState<number>(0);
   const [showCount, setShowCount] = useState<number>(0);
   const [gridItem, setGridItem] = useState<GridItemType[]>([]);
 
   useEffect(() => restAndCreateGrid(), []);
+
+  useEffect(() => {
+    if (playing) {
+      const timer = setInterval(() => {
+        setTimeElapsed(prev => prev + 1); 
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [playing]); 
+  
+  useEffect(() => {
+    if (showCount === 2) {
+      let opened = gridItem.filter(item => item.shown === true);
+      
+      if (opened.length === 2) {
+        let tmpGrid = [...gridItem];
+  
+        if (opened[0].item === opened[1].item) {
+          
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+        } else {
+          setTimeout(() => {
+            let newGrid = [...tmpGrid];
+            for (let i in newGrid) {
+              if (newGrid[i].shown && !newGrid[i].permanentShown) {
+                newGrid[i].shown = false;
+              }
+            }
+            setGridItem(newGrid);
+          }, 1000); 
+        }
+  
+        setGridItem(tmpGrid);
+        setShowCount(0);
+      }
+    }
+  }, [showCount, gridItem]);
+  
+  useEffect(()=>{
+    if(moveCount > 0 && gridItem.every(item => item.permanentShown === true)){
+      setPlaying(false);
+    }
+  },[moveCount, gridItem])
+
   const restAndCreateGrid = () => {
     setTimeElapsed(0);
     setMoveCount(0);
     setShowCount(0);
 
     let tmpGrid: GridItemType[] = [];
-    for(let i = 0; i < (items.length * 2); i++) tmpGrid.push({
-      item: null,
-      shown: false,
-      permanentShown: false,
-    });
+    for (let i = 0; i < (items.length * 2); i++) {
+      tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false,
+      });
+    }
 
-    for(let w = 0; w < 2; w++){
-      for(let i = 0; i > items.length; i++){
-        let pos = -1
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) { 
+        let pos = -1;
         while (pos < 0 || tmpGrid[pos].item !== null) {
-          let pos = Math.floor(Math.random() * (items.length * 2));
+          pos = Math.floor(Math.random() * (items.length * 2)); 
         }
-        
         tmpGrid[pos].item = i;
       }
     }
 
     setGridItem(tmpGrid);
-
     setPlaying(true);
   };
 
   const handleItemClick = (index: number) => {
-
-  }
+    if (playing && index !== null && showCount < 2) {
+      let tmpGrid = [...gridItem];
+  
+      if (tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
+        tmpGrid[index].shown = true;
+        setShowCount(showCount + 1);
+        setMoveCount(prev => prev + 1); 
+      }
+  
+      setGridItem(tmpGrid);
+    }
+  };
+  
 
   return (
     <C.Container>
@@ -56,23 +117,16 @@ const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          <InfoItem label="Tempo" value="00:00" />
-          <InfoItem label="Jogadas" value="0" />
+          <InfoItem label="Tempo" value={Tempo(timeElapsed)} />
+          <InfoItem label="Jogadas" value={moveCount.toString()} />
         </C.InfoArea>
-        <Button
-          label="Reiniciar"
-          icon={RestartIcon}
-          onClick={restAndCreateGrid}
-        />
+        <Button label="Reiniciar" icon={RestartIcon} onClick={restAndCreateGrid} />
       </C.Info>
+
       <C.GridArea>
         <C.Grid>
-          {gridItem.map((item, index)=>(
-            <GridItem 
-              key={index}
-              item={item}
-              onClick={() => handleItemClick(index)}
-            />
+          {gridItem.map((item, index) => (
+            <GridItem key={index} item={item} onClick={() => handleItemClick(index)} />
           ))}
         </C.Grid>
       </C.GridArea>
--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,5 +1,7 @@
 import * as C from "./styles";
-import {GridItemType} from '../../type/gridetipes'
+import {GridItemType} from '../../types/gridetipes';
+import b7svg from '../../svgs/b7.svg'
+import {items} from '../../data/items'
 
 type Props = {
     item: GridItemType,
@@ -8,8 +10,16 @@ type Props = {
 
 export const GridItem = ({item, onClick}: Props) => {
   return (
-    <C.Container onClick={onClick}>
-      ...
+    <C.Container 
+      onClick={onClick} 
+      showBackground={item.permanentShown || item.shown}
+    >
+      {item.permanentShown === false && item.shown === false && 
+        <C.Icon src={b7svg} alt="" opacity={.1}/>
+      }
+      {(item.permanentShown || item.shown) && item.item !== null && 
+        <C.Icon src={items[item.item].icon} alt="" />
+      }
     </C.Container>
   );
 };
