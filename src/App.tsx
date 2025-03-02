c67789d2891ea6271f44fb9c44c3bd2681581c56
Francilino Neto <88691125+Francilino-Netoo@users.noreply.github.com>
2025-03-02 11:13:54 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,49 @@ import Logo from "./assets/devmemory_logo.png";
 import RestartIcon from "./svgs/restart.svg";
 import { InfoItem } from "./components/infoitenm";
 import { Button } from "./button";
+import {GridItemType} from './types/gridetipes';
+import {items} from './data/items';
+import { GridItem } from "./components/GridItem";
 
 const App = () => {
   const [plauing, setPlaying] = useState<boolean>(false);
   const [timeElapsed, setTimeElapsed] = useState<number>(0);
   const [moveCount, setMoveCount] = useState<number>(0);
   const [showCount, setShowCount] = useState<number>(0);
-  const [gridItem, setGridItem] = useState<>([]);
+  const [gridItem, setGridItem] = useState<GridItemType[]>([]);
 
   useEffect(() => restAndCreateGrid(), []);
-  const restAndCreateGrid = () => {};
+  const restAndCreateGrid = () => {
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
+
+    let tmpGrid: GridItemType[] = [];
+    for(let i = 0; i < (items.length * 2); i++) tmpGrid.push({
+      item: null,
+      shown: false,
+      permanentShown: false,
+    });
+
+    for(let w = 0; w < 2; w++){
+      for(let i = 0; i > items.length; i++){
+        let pos = -1
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          let pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        
+        tmpGrid[pos].item = i;
+      }
+    }
+
+    setGridItem(tmpGrid);
+
+    setPlaying(true);
+  };
+
+  const handleItemClick = (index: number) => {
+
+  }
 
   return (
     <C.Container>
@@ -33,7 +66,15 @@ const App = () => {
         />
       </C.Info>
       <C.GridArea>
-        <C.Grid></C.Grid>
+        <C.Grid>
+          {gridItem.map((item, index)=>(
+            <GridItem 
+              key={index}
+              item={item}
+              onClick={() => handleItemClick(index)}
+            />
+          ))}
+        </C.Grid>
       </C.GridArea>
     </C.Container>
   );
